13f2bcd3c4bf11848ea2c02c25ffd8d54cb9813e
D-Fink <dfink4badjuju@gmail.com>
2020-01-16 08:51:03 -0700

    not mvp- something work, some dont

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 
 const server = express();
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use(logger);
+server.use('/api/user', userRouter);
+server.use('/api/post', postRouter);
+server.use(express.json());
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`
+  );
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,46 +2,96 @@ const express = require('express');
 
 const router = express.Router();
 
+const userHub = require('./userDb.js');
+const postHub = require('../posts/postDb.js')
+
 router.post('/', (req, res) => {
-  // do your magic!
+  userHub.insert(req.body)
+  .then(user => {
+    res.status(200).json(user)
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  postHub.insert(req.params.id, req.body)
+  .then(post => {
+    res.status(200).json(post)
+  })
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  userHub.get()
+  .then(users => {
+    res.status(200).json(users)
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  userHub.getById(req.params.id)
+  .then(user => {
+    res.status(200).json(user)
+  })
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  userHub.getUserPosts(req.params.id)
+  .then(posts => {
+    res.status(200).json(posts)
+  })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  userHub.remove(req.params.id)
+  .then(user => {
+    res.status(200).json({message: 'user deleted'})
+  })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, (req, res) => {
+  userHub.update(req.params.id, req.body)
+  .then(user => {
+    res.status(200).json(user)
+  })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  userHub.getById(req.params.id)
+  .then(users => {
+    if(users) {
+      req.user = users;
+      next();
+    }
+    else {
+      res.status(500).json({message: 'cannot find user'})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({message: 'missing id'})
+  })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(req.body) {
+    if(req.body.name){
+      next();
+    }
+    else{
+      res.status(400).json({message: 'name required'})
+    }
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(req.body){
+    if(req.body.text){
+      next();
+    }
+    else {
+      res.status(400).json({message: 'missing text'})
+    }
+  }
 }
 
 module.exports = router;
